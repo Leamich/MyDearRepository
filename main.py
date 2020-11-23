f45cf64ad9e12eb1b723a2501ca971ea933ebba2
Леонтьев Михаи <tennantonelove@yandex.ru>
2020-11-23 20:09:44 +0400

    разноцветные круги теперь... И класс-UI, конечно...

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import sys
 from random import randint
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
 from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtCore import QMetaObject, QCoreApplication
 from PyQt5 import uic
 
 
-class Window(QMainWindow):
+class UiWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(716, 609)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.btn = QPushButton(self.centralwidget)
+        self.btn.setGeometry(250, 550, 191, 41)
+        self.btn.setObjectName("btn")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Красные треугольники"))
+        self.btn.setText(_translate("MainWindow", "Зелёный квадрат"))
+
+
+class Window(QMainWindow, UiWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.need_to_draw = False
 
         self.btn.clicked.connect(self.draw_fig)
@@ -24,7 +45,9 @@ class Window(QMainWindow):
 
         if self.need_to_draw:
             x, y = randint(0, 600), randint(0, 450),
-            qp.setBrush(QColor(255, 255, 0))
+            qp.setBrush(QColor(randint(0, 200),
+                               randint(0, 200),
+                               randint(0, 200)))
             qp.drawEllipse(x, y,
                            randint(10, 700 - x),
                            randint(10, 550 - y))
