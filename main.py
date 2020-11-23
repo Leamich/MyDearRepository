cd5cc5c5f17d2920918419b235e90990354c7573
Леонтьев Михаи <tennantonelove@yandex.ru>
2020-11-23 19:31:30 +0400

    Начальный скелет

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-pass
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    wind = Window()
+    wind.show()
+    sys.exit(app.exec())
